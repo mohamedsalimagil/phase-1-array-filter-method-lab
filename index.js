698818b175e29717c55222941434c3423d9cfd4c
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-06-30 22:26:03 +0300

    phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+const driverNames = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+const driverObjects = [
+  { name: 'Bobby', hometown: 'Pittsburgh' },
+  { name: 'Sammy', hometown: 'New York' },
+  { name: 'Sally', hometown: 'Cleveland' },
+  { name: 'Annette', hometown: 'Los Angeles' },
+  { name: 'Bobby', hometown: 'Tampa Bay' }
+];
+function findMatching(drivers, query) {
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase() === query.toLowerCase();
+  });
+}
+function fuzzyMatch(drivers, query) {
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase().startsWith(query.toLowerCase());
+  });
+}
+
+function matchName(drivers, query) {
+  return drivers.filter(function(driver) {
+    return driver.name === query;
+  });
+}
